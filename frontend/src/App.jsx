a9ef620f05e24c036c36a92133b63fd1a693ce1d
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2026-05-04 05:46:29 +0530

    Fix Vite chunk size warning by implementing React.lazy code splitting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,30 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/index.jsx';
-import Achintya from './pages/achintya.jsx';
-import Gallery from './pages/gallery.jsx';
-import Blog from './pages/blog.jsx';
-import BlogPost from './pages/BlogPost.jsx';
-import Contact from './pages/contact.jsx';
 import ScrollToTop from './components/global/ScrollToTop.jsx';
+import Loader from './components/global/Loader.jsx';
+
+// Code-splitting routes
+const Home = lazy(() => import('./pages/index.jsx'));
+const Achintya = lazy(() => import('./pages/achintya.jsx'));
+const Gallery = lazy(() => import('./pages/gallery.jsx'));
+const Blog = lazy(() => import('./pages/blog.jsx'));
+const BlogPost = lazy(() => import('./pages/BlogPost.jsx'));
+const Contact = lazy(() => import('./pages/contact.jsx'));
 
 export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/achintya" element={<Achintya />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/achintya" element={<Achintya />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
